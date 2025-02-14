db0f16826513cba7d809ffaa01bf0b709a701574
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-02-14 08:08:08 +0500

    заменил tsLoader на Babel

---
+++ config/build/babelLoader.ts
@@ -0,0 +1,19 @@
+import { BuildOptions } from "./types";
+
+export const getBabelConfig = (options: BuildOptions) => {
+  const isDev = options.mode === "development";
+  return {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-typescript",
+          ["@babel/preset-react", { runtime: "automatic" }],
+        ],
+      },
+    },
+  };
+};
